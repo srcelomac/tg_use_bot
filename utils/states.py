2aab7b6f5323a2b53001c67017fd61f40c5c68e2
Дмитрий <143512938+srcelomac@users.noreply.github.com>
2024-06-28 23:24:15 +0300

    Update states.py

--- utils/states.py
+++ utils/states.py
@@ -1,12 +1,24 @@
 from aiogram.fsm.state import StatesGroup, State
 
-class Start(StatesGroup):
-    mode = State()
-    task = State()
+# class Start(StatesGroup):
+#     mode = State()
+#     task = State()
 
-class Main_menu(StatesGroup):
+class Base(StatesGroup):
     mode = State()
     task = State()
     exercise = State()
     svg = State()
     add = State()
+    key = State()
+    step = State()
+    answer_true = State()
+    answer_false = State()
+    flag_training = State()
+    flag_add = State()
+    word_was = State()
+    task_id = State()
+
+class Start(StatesGroup):
+    default = State()
+    send = State()
