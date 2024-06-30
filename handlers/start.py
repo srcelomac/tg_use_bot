ff85bf4c9bcf766fcf9b8f484dc556ff8d3d4123
Дмитрий <143512938+srcelomac@users.noreply.github.com>
2024-07-01 01:06:31 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -2,32 +2,58 @@ from aiogram import Bot, Router, F
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.fsm.context import FSMContext
-from utils.states import Start
+from utils.states import Start, Base
 import keyboard
 import sqlite3
 import math
 import os
 
+async def get_stats(chat_id):
+    conn = sqlite3.connect('stats.db')
+    cur = conn.cursor()
+    cur.execute(f'SELECT COUNT (*) from Stats where user_id = {chat_id}')
+    tmp = str(cur.fetchall()[0])
+    tmp = tmp.replace('(', '')
+    tmp = tmp.replace(')', '')
+    tmp = tmp.replace("'", '')
+    tmp = tmp.replace(',', '')
+    cnt = int(tmp)
+    if (cnt == 0):
+        cur.execute('INSERT INTO Stats (user_id, rights, wrongs) VALUES (?, ?, ?)',
+                    (chat_id, 0, 0))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
 router = Router()
 
 @router.message(Start.send)
 async def total_message(message: Message, state: FSMContext):
+    t = message.text
     conn = sqlite3.connect('stats.db')
     cur = conn.cursor()
     sqlite_select_query = f"""SELECT user_id from Stats"""
     cur.execute(sqlite_select_query)
     users_id = cur.fetchall()
+    print(type(users_id))
     cur.close()
     conn.close()
 
-    for x in users_id:
+    tg_token = os.environ["tg_test_token"]
+    bot = Bot(str(tg_token))
+
+    for i in range(len(users_id)):
+        x = str(users_id[i])
         x = x.replace('(', '')
         x = x.replace(')', '')
         x = x.replace("'", '')
         x = x.replace(',', '')
-        tg_token = os.environ["tg_test_token"]
-        bot = Bot(str(tg_token))
-        await bot.send_message(chat_id=int(x), text=message.text)
+        if (x != ''):
+            try:
+                await bot.send_message(chat_id=int(x), text=t)
+            except Exception as e:
+                print(f"Ошибка при отправке сообщения пользователю {x}: {e}")
 
     await state.set_state(Start.default)
 
@@ -37,6 +63,9 @@ async def start(message: Message, state: FSMContext):
     await message.answer(
         f"Привет, {message.from_user.first_name}!\nЗдесь ты можешь попрактиковаться в орфографии для ЕГЭ.\nДля управления используй встроенную или обычную клавиатуру",
         reply_markup=keyboard.start_kb)
+    await get_stats(message.chat.id)
+    await state.update_data(key=message.chat.id)
+    await state.set_state(Base.mode)
 
 @router.message(F.text.lower() == "/admin")
 async def start(message: Message, state: FSMContext):
